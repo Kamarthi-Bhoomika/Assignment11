34412bd1f0f94e10af034a1a3fe751204bfc6f86
unknown <kbhoomika05@gmail.com>
2023-07-20 17:17:22 +0530

    Assignment11 IDisposable List

---
+++ LargeDataCollection.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment11
+{
+    public class LargeDataCollection : IDisposable
+    {
+        private List<string> list;
+        static int totalLen = 0;
+        bool disposedValue;
+        public LargeDataCollection(string listName) 
+        {
+            
+            if (list == null)
+            {
+                totalLen++;
+                list = new List<string>();
+            }
+            Console.WriteLine("File Created");
+            Console.WriteLine("Number of files are: " + totalLen);
+        }
+        public LargeDataCollection(List<string> list)
+        {
+            this.list = list;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    totalLen = 0;
+                }
+            }
+            Console.WriteLine("List {0} has been disposed", list);
+            list = null;
+            disposedValue = true;
+        }
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        public void Add(string item)
+        {
+            list.Add(item);
+            Console.WriteLine(item + " has been Added Successfully");
+        }
+        public void Read()
+        {
+            Console.WriteLine("************************");
+            Console.WriteLine("List Items");
+            foreach (string items in list)
+            {
+                Console.WriteLine(items);
+            }
+        }
+        public void Remove(string item)
+        {
+            list.Remove(item);
+            Console.WriteLine($"{item} removed from list");
+        }
+        public void getItem(string item)
+        {
+            if (list.Contains(item))
+            {
+                Console.WriteLine($"{item} is available in the list");
+            }
+            else { Console.WriteLine($"{item} is not available in the list"); }
+        }
+        ~LargeDataCollection()
+        {
+            Dispose(false);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>
+            { "Amit","Sam"}; 
+            LargeDataCollection largeDataCollection = new LargeDataCollection(list);
+            largeDataCollection.Read();
+            Console.WriteLine("Enter Element to Add: ");
+            string name = Console.ReadLine();
+            largeDataCollection.Add(name);
+            largeDataCollection.Read();
+            Console.WriteLine("Enter element to remove: ");
+            string remove = Console.ReadLine();
+            largeDataCollection.Remove(remove);
+            largeDataCollection.Read();
+            Console.WriteLine("Enter element to search: ");
+            string get = Console.ReadLine();
+            largeDataCollection.getItem(get);
+            largeDataCollection.Read();
+
+            largeDataCollection.Dispose();
+
+            Console.ReadKey();
+        }
+    }
+}
